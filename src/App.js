7430c1532dab69a71e9d28f78e143beeabb9cfd8
aent <alintssr@gmail.com>
2024-01-29 20:25:15 -0500

    useRef hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
 
 function App() {
+  const [name, setName] = useState("")
+  // const renderCount = useRef(1)
+  // const inputRef = useRef()
+  const prevName = useRef('')
+
+  // function focus() {
+  //   // console.log(inputRef.current)
+  //   inputRef.current.focus()
+  //   inputRef.current.value = 'Some value'
+  // }
+
+  // useEffect(() => {
+  //   // setRenderCount(prevRenderCount => prevRenderCount + 1)
+  //   renderCount.current = renderCount.current + 1
+  // })
+
+  useEffect(() => {
+    prevName.current = name
+  }, [name])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <div>My name is {name} and it used to be {prevName.current}</div>
+      {/* <div>I rendered {renderCount.current} times</div> */}
+      {/* <button onClick={focus}>Focus</button> */}
+    </>
   );
 }
 
 export default App;
+
+// ref is very similar to state in that you can store a previous value in it, and it persists between different renders, 
+// but it does not cause you to re-render like state does.
